02a045def54aaa21b8692c6146010b79ea32140b
pdrozdz933 <pdrozdz933@gmail.com>
2021-03-29 19:31:38 +0200

    lol nie wiem

---
+++ SPOJ/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputValues = Console.ReadLine();
+            short[] valuesArray = Array.ConvertAll<string, short>(inputValues.Split(" "), short.Parse);
+
+            short a = valuesArray[0];
+            short b = valuesArray[1];
+
+            if (a == b)
+            {
+                Console.WriteLine("empty");
+            }
+            if (a - b > 10)
+            {
+                Console.Write($"{b + 1} ,{b + 2} ,{b + 3} ,..., {a - 2}, {a - 1}");
+                while (b == a - 2) ;
+                return;
+            }
+            if (b - a > 10)
+            {
+                Console.Write($"{a + 1}, {a + 2}, {a + 3} ... {b - 2}, {b - 1}");
+                while (a == b - 2) ;
+                return;
+            }
+            if (a < b)
+            {
+                for (int i = a - b; a + 1 < b - 1; a++)
+                {
+                    Console.Write("{0}, ", a + 1);
+                }
+                Console.Write(a + 1);
+            }
+            if (a > b)
+            {
+                for (int i = b - a; a - 1 > b + 1; b++)
+                {
+                    Console.Write("{0}, ", b + 1);
+                }
+                Console.Write(b + 1);
+            }
+        }
+    }
+}
--- SPOJ/spoj1/spoj1/Program.cs
+++ SPOJ/spoj1/spoj1/Program.cs
@@ -1,28 +1,63 @@
 ﻿using System;
 
-namespace spoj1
+public class spoj1
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        string inputValues = Console.ReadLine();
+        short[] valuesArray = Array.ConvertAll<string, short>(inputValues.Split(" "), short.Parse);
+
+        short a = valuesArray[0];
+        short b = valuesArray[1];
+
+        if (a - b == 1)
         {
-            int t = int.Parse(Console.ReadLine());
-            for(int i = 0; i<t; i++)
+            Console.WriteLine("empty");
+        }
+        if (b - a == 1)
+        {
+            Console.WriteLine("empty");
+        }
+        if (a == b)
+        {
+            Console.WriteLine("empty");
+        }
+        if (a - b > 11)
+        {
+            Console.Write($"{b + 1} ,{b + 2} ,{b + 3}, ..., {a - 2}, {a - 1}");
+            while (b == a - 2) ;
+            return;
+        }
+        if (b - a > 11)
+        {
+            Console.Write($"{a + 1}, {a + 2}, {a + 3}, ..., {b - 2}, {b - 1}");
+            while (a == b - 2) ;
+            return;
+        }
+        if (a < b)
+        {
+            for (int i = a + 1; i + 1 <= b; i++)
             {
-                int n = int.Parse(Console.ReadLine());
-                string napis = Console.ReadLine();
-                var tab = napis.Split(' ');
-                int suma = 0;
-                for(int j = 0; j<n; j++)
+                if (i + 1 == b)
                 {
-                    int x = int.Parse(tab[j]);
-                    suma += x;
+                    Console.Write("{0}", i);
+                    break;
                 }
-
-                Console.WriteLine(suma);
-                gf
-
+                Console.Write("{0}, ", i);
+            }
+        }
+        if (a > b)
+        {
+            for (int i = b + 1; a >= i + 1; i++)
+            {
+                if (i + 1 == a)
+                {
+                    Console.Write("{0}", i);
+                    break;
+                }
+                Console.Write("{0}, ", i);
             }
         }
     }
+}
 }
