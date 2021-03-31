c2fa1fbc4a6a2c6e1a4c8191fb855ddbab05f5c0
pdrozdz933 <pdrozdz933@gmail.com>
2021-03-31 08:53:09 +0200

    .

---
+++ zadanie1/z1/z1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace z1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 7;
+            for (int i = 1; i <= 4; i++)
+            {
+                for (int j = 1; j <= 1; j++)
+                {
+                    if (i != j)
+                    {
+                        n--;
+                    }
+                }
+            }
+            Console.Write(n);
+        }
+    }
+}
