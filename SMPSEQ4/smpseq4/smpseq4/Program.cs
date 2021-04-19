7e1ae733e7a6be235837c5f43b759e1e75174fe6
pdrozdz933 <pdrozdz933@gmail.com>
2021-04-19 12:51:53 +0200

    .

---
+++ SMPSEQ4/smpseq4/smpseq4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace smpseq4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            string c = Console.ReadLine();
+            string d = Console.ReadLine();
+            string[] sSplit = b.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] qSplit = d.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int x = 0; x < sSplit.Length; x++)
+            {
+                bool found = true;
+                for (int y = 0; y < qSplit.Length; y++)
+                {
+                    if (sSplit[x] == qSplit[y])
+                    {
+                        found = false;
+                    }
+                }
+                if (!found)
+                {
+                    Console.Write(sSplit[x] + " ");
+                }
+            }
+        }
+    }
+}
