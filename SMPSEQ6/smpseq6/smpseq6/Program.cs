bbab8778f52338f1f98fcef78816b402c6baa725
pdrozdz933 <pdrozdz933@gmail.com>
2021-04-20 20:41:08 +0200

    .

---
+++ SMPSEQ6/smpseq6/smpseq6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace smpseq6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            string c = Console.ReadLine();
+            string[] aSplit = a.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] bSplit = b.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] cSplit = c.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int n = Int32.Parse(aSplit[0]);
+            int x = Int32.Parse(aSplit[1]);
+            if (x >= 0)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int y = -x; y <= x; y++)
+                    {
+                       if (i + y < n)
+                       {
+                         if (i + y >= 0)
+                         {
+                           if (bSplit[i] == cSplit[i + y])
+                           {
+                              Console.Write($"{i + 1}" + " ");
+                              break;
+                           }
+                         }
+                       }
+                    }
+                }
+            }
+            else
+            {
+                Console.Write("");
+            }
+        }
+    }
+}
