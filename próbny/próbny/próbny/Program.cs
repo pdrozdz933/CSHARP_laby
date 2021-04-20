1966dbf22faf3cf5a667439dc2180202c203f05b
pdrozdz933 <pdrozdz933@gmail.com>
2021-04-20 22:54:43 +0200

    .

---
+++ próbny/próbny/próbny/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace próbny
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            int n = Int32.Parse(a);
+
+            if (n <= 3)
+            {
+                Console.WriteLine("error");
+                return;
+            }
+            if (n % 2 == 0)
+            {
+                n--;
+            }
+            for (int i = 0; i < ((n+1)/2); i++) //numer wiersza
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i > j || n-i<=j)
+                    {
+                        Console.Write(" ");
+                    }
+                    else
+                    {
+                        Console.Write("*");
+                    }
+                }
+                Console.WriteLine();
+            }
+
+        }   
+    }
+
+}
---
+++ próbny/z3/z3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace z3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            
+                string a = Console.ReadLine();
+                string b = Console.ReadLine();
+                string c = Console.ReadLine();
+
+
+            int d = Int32.Parse(a);
+            int e = Int32.Parse(b);
+            int f = Int32.Parse(c);
+
+            int g = d + e + f;
+
+            Console.WriteLine($"{g}");
+            
+
+        }
+    }
+}
