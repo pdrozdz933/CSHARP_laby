99557a70b524bd301f57365387a1f3100a58ba0e
pdrozdz933 <pdrozdz933@gmail.com>
2021-04-21 17:17:08 +0200

    .

---
+++ SMPSEQ8/SMPSEQ8/SMPSEQ8/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace SMPSEQ8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arrayOne = Console.ReadLine().Split(' ').Take(n).Select(int.Parse).ToArray();
+            int m = int.Parse(Console.ReadLine());
+            int[] arrayTwo = Console.ReadLine().Split(' ').Take(m).Select(int.Parse).ToArray();
+
+            arrayOne.Sum();
+            arrayTwo.Sum();    
+            
+            if (arrayOne.Length >= 2 && arrayOne.Length <= 100 && arrayTwo.Length >= 2 && arrayTwo.Length <= 100)
+            {
+                for (int i = 0; i < arrayOne.Length; i++)
+                {
+                    arrayOne.Sum();                 
+                    break;
+                }
+                for (int j = 0; j < arrayTwo.Length; j++)
+                {
+                    arrayTwo.Sum();                  
+                    break;
+                }
+
+
+                if (arrayOne.Sum() > arrayTwo.Sum())
+                {
+                    Console.WriteLine(string.Join(' ', arrayOne));
+                }
+                else
+                {
+                    Console.WriteLine(string.Join(' ', arrayTwo));
+                }
+            }
+
+        }
+    }
+}
---
+++ próbny/z.2/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public static double TriangleArea(int a, int b, int c, int precision = 2)
+        {
+
+    
+
+            
+
+           
+                   if(a>0 || b>0 || c>0 || precision<2 || precision >8)
+                   {
+                        Console.WriteLine("Wrong arguments");
+                        throw new ArgumentOutOfRangeException();
+                            
+                   }
+                   
+           
+                   if(a+b<c || a+c<b || b+c<a)
+                   {
+                     Console.WriteLine("object not exists");
+                     throw new ArgumentException();
+                   }
+                            
+                
+                    double p = ((a + b + c) / 2);
+                    double pole = (double)(p*((p-a)*(p-b)*(p-c)));
+                    return Math.Round(pole, precision);
+
+            
+            
+                
+                
+            
+            
+      
+               
+            
+           
+        }
+    }
+}
